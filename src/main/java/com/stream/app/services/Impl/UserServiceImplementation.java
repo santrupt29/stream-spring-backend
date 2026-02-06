42a3d1c8c5b321fcc126735beaea1787c7e6cfb3
Santrupt Potphode <santrupt.potphode29@gmail.com>
2026-02-06 18:10:30 +0530

    Login functionality using JWT implemented

--- src/main/java/com/stream/app/services/Impl/UserServiceImplementation.java
+++ src/main/java/com/stream/app/services/Impl/UserServiceImplementation.java
@@ -2,8 +2,11 @@ package com.stream.app.services.Impl;
 
 import com.stream.app.entities.Users;
 import com.stream.app.repositories.UserRepo;
+import com.stream.app.services.JWTService;
 import com.stream.app.services.UserService;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -12,15 +15,12 @@ public class UserServiceImplementation implements UserService {
 
     private UserRepo repo;
     AuthenticationManager authManager;
-    public UserServiceImplementation(UserRepo repo, AuthenticationManager authManager) {
+    private JWTService jwtService;
+    public UserServiceImplementation(UserRepo repo, AuthenticationManager authManager, JWTService jwtService) {
         this.authManager = authManager;
         this.repo = repo;
+        this.jwtService = jwtService;
     }
-
-
-
-
-
 
     private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
 
@@ -32,6 +32,8 @@ public class UserServiceImplementation implements UserService {
 
     @Override
     public String verify(Users user) {
-        return "";
+        Authentication authentication = authManager.authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
+        if(authentication.isAuthenticated()) return jwtService.generateToken(user.getUsername());
+        return "Failure";
     }
 }
