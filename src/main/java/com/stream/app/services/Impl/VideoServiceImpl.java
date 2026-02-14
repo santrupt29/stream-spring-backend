a3d1c85cc596dec95fd6f97e1800c8c2ca4f335b
Santrupt Potphode <santrupt.potphode29@gmail.com>
2026-02-14 16:14:03 +0530

    Implemented DigitalOcean Space for storing hls segments

--- src/main/java/com/stream/app/services/Impl/VideoServiceImpl.java
+++ src/main/java/com/stream/app/services/Impl/VideoServiceImpl.java
@@ -1,13 +1,18 @@
 package com.stream.app.services.Impl;
 
 import ch.qos.logback.core.util.StringUtil;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.stream.app.entities.Video;
 import com.stream.app.repositories.VideoRepository;
 import com.stream.app.services.VideoService;
 import jakarta.annotation.PostConstruct;
 import org.springframework.aop.scope.ScopedProxyUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.util.FileSystemUtils;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.multipart.MultipartFile;
@@ -21,6 +26,7 @@ import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Stream;
 
 @Service
 public class VideoServiceImpl implements VideoService {
@@ -31,65 +37,44 @@ public class VideoServiceImpl implements VideoService {
     @Value("${file.video.hsl}")
     String HSL_DIR;
 
+    @Autowired
+    private AmazonS3 s3Client;
+
+    @Value("${cloud.aws.bucket.name}") private String bucketName;
+    @Value("${cloud.aws.cdn.url}") private String cdnUrl;
+
     private VideoRepository videoRepository;
 
     public VideoServiceImpl(VideoRepository videoRepository) {
         this.videoRepository = videoRepository;
     }
 
-    @PostConstruct
-    public void init() {
-        File file = new File(DIR);
-
-        try {
-            Files.createDirectories(Paths.get(HSL_DIR));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        if(!file.exists()) {
-            file.mkdir();
-            System.out.println("Folder created");
-        } else {
-            System.out.println("Folder already created");
-        }
-    }
-
     @Override
     public Video save(Video video, MultipartFile file) {
-        try {
-            String filename = file.getOriginalFilename();
-            String contentType = file.getContentType();
-            InputStream inputStream = file.getInputStream();
+        String videoId = UUID.randomUUID().toString();
+        video.setVideoId(videoId);
 
-            assert filename != null;
-            String cleanFileName = StringUtils.cleanPath(filename);
-            String cleanFolder = StringUtils.cleanPath(DIR);
-            Path path = Paths.get(cleanFolder, cleanFileName);
-
-            System.out.println(path);
-
-            Files.copy(inputStream, path, StandardCopyOption.REPLACE_EXISTING);
-
-            video.setContentType(contentType);
-            video.setFilePath(path.toString());
-            videoRepository.save(video);
-
-            try {
-                processVideo(video.getVideoId());
-                return video;
-
-            } catch (Exception e) {
-                videoRepository.delete(video);
-                System.out.println("Error in processing video");
-                System.out.println(e);
-            }
-
+        Path tempRawPath = Paths.get(System.getProperty("java.io.tmpdir"), videoId + "_raw.mp4");
+        try (InputStream inputStream = file.getInputStream()) {
+            Files.copy(inputStream, tempRawPath, StandardCopyOption.REPLACE_EXISTING);
         } catch (IOException e) {
-            e.printStackTrace();
-            return null;
+            throw new RuntimeException("Could not store raw file", e);
         }
-        return null;
+
+        video.setContentType(file.getContentType());
+        video.setFilePath(tempRawPath.toString());
+
+        // saveAndFlush to ensure the ID is in the DB before FFmpeg starts
+        Video savedVideo = videoRepository.saveAndFlush(video);
+
+        try {
+            processVideo(savedVideo.getVideoId());
+            return savedVideo;
+        } catch (Exception e) {
+            videoRepository.delete(savedVideo);
+            try { Files.deleteIfExists(tempRawPath); } catch (IOException ignored) {}
+            throw new RuntimeException("Video processing failed: " + e.getMessage());
+        }
     }
 
     @Override
@@ -111,45 +96,54 @@ public class VideoServiceImpl implements VideoService {
     @CrossOrigin("*")
     @Override
     public String processVideo(String videoId) {
-        Video video = this.get(videoId);
-        String filePath = video.getFilePath();
-
-        Path videoPath = Paths.get(filePath);
-//        String output360p = HSL_DIR+videoId+"/360p";
-//        String output720p = HSL_DIR+videoId+"/720p";
-//        String output1080p = HSL_DIR+videoId+"/1080p";
-
+        Video video = videoRepository.findById(videoId).orElseThrow();
+        Path rawVideoPath = Paths.get(video.getFilePath());
+        Path hlsOutputDir = Paths.get(System.getProperty("java.io.tmpdir"), videoId);
 
         try {
-//            Files.createDirectories(Paths.get(output360p));
-//            Files.createDirectories(Paths.get(output720p));
-//            Files.createDirectories(Paths.get(output1080p));
-
-            Path outputPath = Paths.get(HSL_DIR, videoId);
-            Files.createDirectories(outputPath);
+            Files.createDirectories(hlsOutputDir);
 
             String ffmpegCmd = String.format(
                     "ffmpeg -i \"%s\" -c:v libx264 -c:a aac -strict -2 -f hls -hls_time 10 -hls_list_size 0 -hls_segment_filename \"%s/segment_%%3d.ts\"  \"%s/master.m3u8\" ",
-                    videoPath, outputPath, outputPath
+                    rawVideoPath, hlsOutputDir, hlsOutputDir
             );
 
-            System.out.println(ffmpegCmd);
-            ProcessBuilder processBuilder = new ProcessBuilder("/bin/bash", "-c", ffmpegCmd);
-            processBuilder.inheritIO();
-            Process process = processBuilder.start();
-            int exit = process.waitFor();
-            if (exit != 0) {
-                throw new RuntimeException("video processing failed!!");
+            Process process = new ProcessBuilder("/bin/bash", "-c", ffmpegCmd).inheritIO().start();
+            if (process.waitFor() != 0) throw new RuntimeException("FFmpeg failed");
+
+            try (Stream<Path> paths = Files.walk(hlsOutputDir)) {
+                paths.filter(Files::isRegularFile).forEach(path -> {
+                    String fileName = path.getFileName().toString();
+                    String s3Key = "hls/" + videoId + "/" + fileName;
+
+                    com.amazonaws.services.s3.model.ObjectMetadata metadata = new com.amazonaws.services.s3.model.ObjectMetadata();
+
+                    if (fileName.endsWith(".m3u8")) {
+                        metadata.setContentType("application/x-mpegURL");
+                    } else if (fileName.endsWith(".ts")) {
+                        metadata.setContentType("video/MP2T");
+                    }
+
+                    PutObjectRequest request = new PutObjectRequest(bucketName, s3Key, path.toFile())
+                            .withMetadata(metadata)
+                            .withCannedAcl(CannedAccessControlList.PublicRead);
+
+                    s3Client.putObject(request);
+                });
             }
 
+            String finalHlsUrl = cdnUrl + "/hls/" + videoId + "/master.m3u8";
+            video.setFilePath(finalHlsUrl);
+            videoRepository.save(video);
+
             return videoId;
-        } catch (IOException e) {
-            throw new RuntimeException("Video processing Failed", e);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+        } catch (Exception e) {
+            throw new RuntimeException("Processing Failed", e);
+        } finally {
+            // clean up local files
+            FileSystemUtils.deleteRecursively(hlsOutputDir.toFile());
+            try { Files.deleteIfExists(rawVideoPath); } catch (IOException ignored) {}
         }
-
-
     }
 
     @Override
